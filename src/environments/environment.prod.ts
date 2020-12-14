c75b0a9a24dfd945bd1fe75e56fa8299836309cd
= <=>
2020-12-13 23:40:30 -0500

    fix url prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-johnvanegas-loader-invoice.herokuapp.com/'
+  apiUrl: 'https://api-johnvanegas-loader-invoice.herokuapp.com'
 };
